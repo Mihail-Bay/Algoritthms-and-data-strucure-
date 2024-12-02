e4088e68137d5eeb3810da790466c636f849064e
Бай Михаил Олегович <baymisha@niuitmo.ru>
2024-12-02 21:51:11 +0300

    Lab3

--- Lab4/task1/src/Stack.py
+++ Lab4/task1/src/Stack.py
@@ -1,19 +1,25 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 from Lab4.utils import read_input, write_output
 
-def stack_commands(task):
-    commands = read_input(task)
-    n = int(commands[0])
+def stack_commands(commands, n ):
     stack = []
     out = []
-    for i in commands[1: n + 1]:
-        if i[0] == '+':
-            _, nuber = i.split()
-            stack.append(nuber)
-        elif i[0] == '-':
-            if stack:
-                out.append(stack.pop())
+    for i in commands[1: n + 2]:
+        if i[0] in '+-':
+            if i[0] == '+':
+                _, nuber = i.split()
+                stack.append(nuber)
+            elif i[0] == '-':
+                if stack:
+                    out.append(stack.pop())
     output = '\n'.join(out)
-    write_output(task, output)
+    return output
 
 if __name__ == '__main__':
-    stack_commands(task=1)
+    commands = read_input(task=1)
+    n = int(commands[0])
+    out = stack_commands(commands, n)
+    write_output(1, out)
